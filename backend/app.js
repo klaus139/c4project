9e092f38b5788cd54a7b38f87fec18c91e33ef48
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2024-04-19 21:10:53 +0100

    chnaged env

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,15 @@ import { dbConnection } from "./database/dbConnection.js";
 import jobRouter from "./routes/jobRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import applicationRouter from "./routes/applicationRoutes.js";
-import { config } from "dotenv";
+import dotenv from "dotenv";
+dotenv.config();
 import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 
 const app = express();
-config({ path: "./config/config.env" });
+
 
 app.use(
   cors({
